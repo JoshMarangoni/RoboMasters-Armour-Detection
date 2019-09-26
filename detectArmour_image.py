c31f1c6d6bc93ca09b062d8189987def424c4f16
Josh <joshua.marangoni@gmail.com>
2019-09-25 17:38:51 -0700

    measures slope

--- detectArmour_image.py
+++ detectArmour_image.py
@@ -1,9 +1,11 @@
-# import the necessary packages
+# usage
+# python detectArmour_image.py -i dataset/RoboMasterLabelledImagesSet1/image-550.jpg --save ./
+
 from shapeDetector import ShapeDetector
 import argparse
 import imutils
 import cv2
- 
+
 # construct the argument parse and parse the arguments
 ap = argparse.ArgumentParser()
 ap.add_argument("-i", "--image", required=True,
@@ -13,14 +15,18 @@ ap.add_argument("--save", required=True,
 args = vars(ap.parse_args())
 
 def saveToDisk(img, imgName): 
-	path = args["save"] + '/' + imgName + '.jpg'
+	idx = imgName.find('image-')
+	if args['image'][len(args['image'])-1] == '/':
+		path = args["save"] + imgName[idx:]
+	else: 
+		path = args["save"] + '/' + imgName[idx:]
 	cv2.imwrite(path, img)
 
 # load the image and resize it to a smaller factor so that
 # the shapes can be approximated better
 image = cv2.imread(args["image"])
 image = imutils.resize(image, width=600)
- 
+
 # find saliency map of image, then threshold the LEDs
 saliency = cv2.saliency.StaticSaliencyFineGrained_create()
 (success, saliencyMap) = saliency.computeSaliency(image)
@@ -29,8 +35,15 @@ saliencyMap = (saliencyMap * 255).astype("uint8")
 cv2.imshow("saliency", saliencyMap)
 cv2.waitKey(0)
 
+#gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+#blurred = cv2.GaussianBlur(gray, (5, 5), 0)
+#thresh = cv2.threshold(blurred, 250, 255,cv2.THRESH_BINARY)[1]
+
 thresh = cv2.threshold(saliencyMap.astype("uint8"), 210, 255,
 	cv2.THRESH_BINARY)[1]
+
+#cv2.imshow("blurred", blurred)
+#cv2.waitKey(0)
 
 cv2.imshow("thresh", thresh)
 cv2.waitKey(0)
@@ -43,7 +56,6 @@ cnts = imutils.grab_contours(cnts)
 # initialize shape detector, rectangle count, and list of rectangle 
 # coordinates
 sd = ShapeDetector()
-numRect = 0
 rectangles = []
 
 # loop over the contours
@@ -65,22 +77,42 @@ for c in cnts:
 		c = c.astype("float")
 		c = c.astype("int")
 		cv2.drawContours(image, [c], -1, (0, 255, 0), 2)
-		numRect += 1
 		coord = [cX, cY]
 		rectangles.append(coord)
 
 		# show the output image
 		cv2.imshow("Image", image)
 		cv2.waitKey(0)
-    
-	if numRect == 2: 
-		# average the two rectangles' center coordinates
-		targetcX = int((rectangles[0][0] + rectangles[1][0]) / 2.0)
-		targetcY = int((rectangles[0][1] + rectangles[1][1]) / 2.0)
-		cv2.circle(image, (targetcX, targetcY), 3, (0, 255, 0), -1)
-		cv2.putText(image, "center", (targetcX-25, targetcY+20), 
-			cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
-		# show the output image
-		cv2.imshow("Image", image)
-		cv2.waitKey(0)
-		saveToDisk(image, "target")
+
+print(len(rectangles))
+
+if len(rectangles) > 1: 
+	i = 0
+	while i < len(rectangles)-1:
+		# determine if two rectangles exist with a flat slope between them
+		dx = abs(rectangles[i][0] - rectangles[i+1][0])
+		print(dx)
+		dy = abs(rectangles[i][1] - rectangles[i+1][1])
+		print(dy)
+		slope = float(dy/dx)
+
+		print(slope)
+		print(dx/dy)
+
+		# if slope is close to flat, then matching rectangles found
+		if (slope < 0.3 or (slope > 0.7 and slope < 1.3)): 
+			# average the two rectangles' center coordinates
+			targetcX = int((rectangles[i][0] + rectangles[i+1][0]) / 2.0)
+			targetcY = int((rectangles[i][1] + rectangles[i+1][1]) / 2.0)
+
+			cv2.circle(image, (targetcX, targetcY), 3, (0, 255, 0), -1)
+			cv2.putText(image, "center", (targetcX-25, targetcY+20), 
+				cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
+
+			# show the output image
+			cv2.imshow("Image", image)
+			cv2.waitKey(0)
+			saveToDisk(image, args['image'])
+			break
+
+		i+=1
